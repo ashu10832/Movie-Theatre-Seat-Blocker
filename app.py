e8ce45979dfdb54a66f17200bd92195413d11def
ashu10832 <ashu10832@gmail.com>
2020-01-23 15:25:22 +0530

    Added delays

--- app.py
+++ app.py
@@ -10,21 +10,27 @@ import os
 import sys
 import psutil
 import logging
-from datetime import datetime, date
+from datetime import datetime
+import random
 
 
 # TODO: Use proxies and add random delays to avoid blocking!
 
-## Specify the details of the movie and theatre and showtime here!
+# Specify the details of the movie and theatre and showtime here!
 
-THEATRE = 'm2k rohini'
-ROW = 'B'
-SEAT_NUMBER = '17'
+THEATRE = 'tdi moti nagar'
+ROW = 'A'
+SEAT_NUMBER = '7'
 MOVIE = 'Tanhaji: The Unsung Warrior (UA)'
-SHOWTIME = '5:25'
-REPEAT_TIME = 15*60
-DATE = '21'
+SHOWTIME = '12:50'
+DATE = '22'
 TYPE = '2D'
+
+
+REPEAT_TIMING_LIST = [9,10,11,12,13]
+REPEAT_TIME = random.choice(REPEAT_TIMING_LIST) * 60
+
+SMALL_SLEEPS_LIST = [1.5, 1.8, 2.2, 2.5, 2.8]
 
 
 # Restarts the this script after closing the process
@@ -66,9 +72,9 @@ running = True
 
 option = webdriver.ChromeOptions()
 # Does not open a chrome window when using this
-#option.add_argument("--headless")
+# option.add_argument("--headless")
 # Disables use of GPU to save memory
-#option.add_argument("--disable-gpu")
+# option.add_argument("--disable-gpu")
 # Uses the incognito mode for Chrome
 option.add_argument("--incognito")
 
@@ -82,9 +88,11 @@ try:
     WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='navbar']/div[2]/div/div[2]/div")))
     print("Homepage Loaded")
     searchbox = browser.find_element_by_xpath('//*[@id="input-search-box"]')
+    time.sleep(random.choice(SMALL_SLEEPS_LIST))
     searchbox.send_keys(Keys.ENTER)
+    time.sleep(random.choice(SMALL_SLEEPS_LIST))
     searchbox.send_keys(THEATRE)
-    time.sleep(2)
+    time.sleep(5)
     searchbox.send_keys(Keys.ENTER)
 except TimeoutException:
     print("Timed out waiting for page to load")
@@ -105,6 +113,7 @@ if(DATE == curSelectedDate):
     pass
 else:
     dateEle = browser.find_element_by_xpath("//*[@id='showDates']//li/a/div[contains(text(),'%s')]"%(DATE))
+    time.sleep(random.choice(SMALL_SLEEPS_LIST))
     dateEle.click()
     try:
         WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='showEvents']/ul")))
@@ -115,7 +124,6 @@ else:
         print("Timed out waiting for page to load")
         browser.quit()
 
-# TODO: Handle the options of 2D and 3D movies with same name
 
 showTime = browser.find_element_by_xpath("//*[@id='showEvents']//a[contains(text(),'%s')]//ancestor::div[@class='listing-info']/div[@class='eventInfo'][contains(text(),'%s')]//ancestor::li[@class='list']/div[2]//div[contains(text(),'%s')]/ancestor::a"%(MOVIE,TYPE,SHOWTIME))
 if 'data-disabled' in showTime.get_attribute("class"):
@@ -124,28 +132,32 @@ if 'data-disabled' in showTime.get_attribute("class"):
     browser.quit()
     exit()
 
-print('Showtime: ',showTime.text,'selected!')
+print('Showtime: ', showTime.text, 'selected!')
+time.sleep(random.choice(SMALL_SLEEPS_LIST))
 browser.execute_script("arguments[0].click();", showTime)
 
 # TODO: For some theatres, terms and conditions is not shown and directly number of seats are chosen, handle that case
 acceptButton = browser.find_element_by_xpath("//*[@id='btnPopupAccept']")
+time.sleep(random.choice(SMALL_SLEEPS_LIST))
 browser.execute_script("arguments[0].click();", acceptButton)
 print("Accepted the terms!")
 
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='pop_1']")))
-    singleSeat = browser.find_element_by_xpath("//*[@id='pop_1']")
-    singleSeat.click()
-    print("Single Seat Clicked!")
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    browser.quit()
-except NoSuchElementException:
-    print("Unable to locate the number of seat option")
-    stop_program()
+# try:
+#     WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='pop_1']")))
+#     singleSeat = browser.find_element_by_xpath("//*[@id='pop_1']")
+#     time.sleep(random.choice(SMALL_SLEEPS_LIST))
+#     singleSeat.click()
+#     print("Single Seat Clicked!")
+# except TimeoutException:
+#     print("Timed out waiting for page to load")
+#     browser.quit()
+# except NoSuchElementException:
+#     print("Unable to locate the number of seat option")
+#     stop_program()
 
 
 proceedToSeats = browser.find_element_by_xpath("//*[@id='proceed-Qty']")
+time.sleep(random.choice(SMALL_SLEEPS_LIST))
 browser.execute_script("arguments[0].click();", proceedToSeats)
 print("Proceed to Seats!")
 
@@ -158,12 +170,12 @@ try:
     availability = seat.get_attribute("class")
     if "available" in availability:
         # Book the seat
+        time.sleep(random.choice(SMALL_SLEEPS_LIST))
         book_seat()
         print(datetime.now())
     elif "blocked" in availability:
         print("Sorry! This seat is already booked! Try another.")
         stop_program()
-
 
 except TimeoutException:
     print("Timed out waiting for page to load - Seat Chart")
@@ -174,7 +186,13 @@ except NoSuchElementException:
     stop_program()
 
 
-time.sleep(REPEAT_TIME)
+for remaining in range(REPEAT_TIME, 0, -1):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+    sys.stdout.flush()
+    time.sleep(1)
+
+print("\nCancelling Booking!")
 cancel_booking()
 browser.quit()
 restart_program()
